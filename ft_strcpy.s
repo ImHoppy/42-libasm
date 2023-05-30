9599454ab497caeaf7ce872dcd3c1b764f6f9405
Hoppy <mbraets@student.42.fr>
2023-05-30 16:23:49 +0200

    Fix overlap register in strcpy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -6,13 +6,14 @@ ft_strcpy:						; rdi = dest ; rsi = src
 loop_cpy:
 	cmp		BYTE [rsi + rcx], 0x0	; rsi[rcx] == '\0'
 	je		return				; Break loop if condition is true
-	mov		cl, [rsi + rcx]		; Get src char at rcx
-	mov		[rdi + rcx], cl		; Cpy char from rsi[rcx] at rdi[rcx]
+	mov		bl, [rsi + rcx]		; Get src char at rcx
+	mov		[rdi + rcx], bl		; Cpy char from rsi[rcx] at rdi[rcx]
 	inc		rcx					; Increment rcx
 	jmp		loop_cpy
 
 return:
-	mov		cl, 0x0				; Set '\0' to cl
-	mov		[rdi + rcx], cl		; Terminate rdi by NULL byte
+
+	mov		bl, 0x0				; Set '\0' to bl
+	mov		[rdi + rcx], bl		; Terminate rdi by NULL byte
 	mov 	rax, rdi			; Move rdi to rax as return value
 	ret
