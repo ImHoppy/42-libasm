5207b2cd95a3a8442b29f11033b626f32b10ed36
Hoppy <mbraets@student.42.fr>
2023-05-31 14:10:26 +0200

    add ft_read test

--- main.c
+++ main.c
@@ -7,14 +7,29 @@ int ft_strlen(const char *);
 char *ft_strcpy(char *, const char *);
 int ft_strcmp(const char *, const char *);
 ssize_t ft_write(int fd, const void *buf, size_t count);
+ssize_t ft_read(int fd, void *buf, size_t count);
 
 void test_ft_write(int fd, char *b, size_t len)
 {
+	printf("----\n");
+
 	const int ret_write = write(fd, b, len);
 	printf("write %d, errno = %d\n", ret_write, errno);
 	errno = 0;
 	const int ret_ft_write = ft_write(fd, b, len);
 	printf("ft_write %d, errno = %d\n", ret_ft_write, errno);
+	errno = 0;
+}
+
+void test_ft_read(int fd, char *buf, size_t count)
+{
+	printf("----\n");
+
+	const int ret_read = read(fd, buf, count);
+	printf("read %d, errno = %d\n", ret_read, errno);
+	errno = 0;
+	const int ret_ft_read = ft_read(fd, buf, count);
+	printf("ft_read %d, errno = %d\n", ret_ft_read, errno);
 	errno = 0;
 }
 
@@ -26,14 +41,19 @@ int main()
 	printf("dst = %s %s\n", dst, b);
 	printf("len = %d\n", ft_strlen(dst));
 
+	getchar();
 	printf("ft_strcmp = %d strcmp = %d\n", ft_strcmp(a, b), strcmp(a, b));
 	printf("ft_strcmp = %d strcmp = %d\n", ft_strcmp(a, "Hello world  "), strcmp(a, "Hello world  "));
 
-	printf("----\n");
+	getchar();
 	test_ft_write(4, "", 1);
-	printf("----\n");
 	test_ft_write(2, "Hi\n", 3);
-	printf("----\n");
 	test_ft_write(2, NULL, 1);
+
+	getchar();
+	char buf[64];
+	test_ft_read(3, buf, 63);
+	test_ft_read(0, buf, 63);
+	test_ft_read(0, NULL, 63);
 	return 0;
 }
