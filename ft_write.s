10f5cad9fa94c41c389d0b55ff87ac83c6845047
Hoppy <mbraets@student.42.fr>
2023-05-31 12:50:39 +0200

    Save errno on ft_write error

--- ft_write.s
+++ ft_write.s
@@ -1,7 +1,19 @@
 global ft_write
+extern __errno_location
 ; rax 	System call 	rdi 				rsi 		rdx
 ; 1 	sys_write 		#filedescriptor 	$buffer 	#count
+
 ft_write:
 	mov		rax, 1
-	syscall 				; Call sus_write
+	syscall 								; Call sus_write, return errno in negative.
+	cmp		rax, 0							; Compare return of sys_write with 0
+	jl		return_error					; Jump to return_error if rax < 0
+	ret										; return sys_write output
+
+return_error:
+	mov		r8, rax							; Save errno
+	call	__errno_location wrt ..plt		; Get ptr of errno, "wrt ..plt" alow write on readonly variale
+	neg		r8								; set errno as positive
+	mov		[rax], r8						; Set return value of write into *errno
+	mov		rax, -1							; return -1
 	ret
--- main.c
+++ main.c
@@ -8,6 +8,16 @@ char *ft_strcpy(char *, const char *);
 int ft_strcmp(const char *, const char *);
 ssize_t ft_write(int fd, const void *buf, size_t count);
 
+void test_ft_write(int fd, char *b, size_t len)
+{
+	const int ret_write = write(fd, b, len);
+	printf("write %d, errno = %d\n", ret_write, errno);
+	errno = 0;
+	const int ret_ft_write = ft_write(fd, b, len);
+	printf("ft_write %d, errno = %d\n", ret_ft_write, errno);
+	errno = 0;
+}
+
 int main()
 {
 	const char *a = "Hello world!";
@@ -18,10 +28,12 @@ int main()
 
 	printf("ft_strcmp = %d strcmp = %d\n", ft_strcmp(a, b), strcmp(a, b));
 	printf("ft_strcmp = %d strcmp = %d\n", ft_strcmp(a, "Hello world  "), strcmp(a, "Hello world  "));
-	const int ret_write = write(4, b, ft_strlen(b));
-	printf("write %d, errno = %d\n", ret_write, errno);
-	errno = 0;
-	const int ret_ft_write = ft_write(4, b, ft_strlen(b));
-	printf("ft_write %d, errno = %d\n", ret_ft_write, errno);
+
+	printf("----\n");
+	test_ft_write(4, "", 1);
+	printf("----\n");
+	test_ft_write(2, "Hi\n", 3);
+	printf("----\n");
+	test_ft_write(2, NULL, 1);
 	return 0;
 }
