f229d79e6315d08fce748fcefca8c6f06e8ce741
Hoppy <mbraets@student.42.fr>
2023-06-12 16:03:13 +0200

    Revert clean, causing segfault

--- ft_strdup.s
+++ ft_strdup.s
@@ -5,10 +5,9 @@ extern ft_strcpy
 extern malloc
 
 ft_strdup:						; rdi = src
-	push 	rsi					; Push rsi for not causing bug in parent caller
 	push	rdi					; save str for later
 	call	ft_strlen			; get length of str
-	inc		rax					; len + 1 for end \0
+	inc		rax					; len + 1 for \0 end
 	mov		rdi, rax			; rdi = str length
 	call	malloc wrt ..plt	; malloc(rdi)
 	cmp		rax, 0				; if malloc return == NULL
@@ -16,11 +15,9 @@ ft_strdup:						; rdi = src
 	mov		rdi, rax			; rdi = new_str
 	pop		rsi					; rsi = str
 	call	ft_strcpy			; ft_strcpy(rdi, rsi)
-	pop		rsi					; Clean up
 	ret
 
 failure:
 	pop		rdi					; clean
-	pop		rsi					; clean
 	mov		rax, 0				; retrn 0
 	ret
