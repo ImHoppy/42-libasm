874fde93bbd330396143e7c75190de4e26c67279
Hoppy <mbraets@student.42.fr>
2023-06-12 17:32:24 +0200

    Add comment set new to old list

--- ft_list_push_front_bonus.s
+++ ft_list_push_front_bonus.s
@@ -9,11 +9,12 @@ ft_list_push_front: ; rdi = t_list **begin_list, rsi = void *data
 	mov		rdi, 16				; rdi = sizeof(t_list) = 16
 	call	malloc wrt ..plt
 	pop		rdi
-	cmp		rax, 0
-	je		return
+	cmp		rax, 0				; Check if malloc return 0
+	je		return				; 	-> return == 1
 	mov		[rax], rsi			; new.data = data
 	mov		rcx, [rdi]			; rcx = *begin_list
-	mov		[rax+8], rcx		; new.next = *begin_list
+	mov		[rax + 8], rcx		; new.next = *begin_list
+	mov		[rdi], rax			; *begin_list = new
 
 return:
 	ret
