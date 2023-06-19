a1733442aa26f4dc141d8530704916a158b95a28
Hoppy <mbraets@student.42.fr>
2023-06-19 15:22:22 +0200

    Enhance comment

--- ft_list_push_front_bonus.s
+++ ft_list_push_front_bonus.s
@@ -5,12 +5,14 @@ extern malloc
 ; t_list = { .data = void*, .next = t_list* }
 
 ft_list_push_front: ; rdi = t_list **begin_list, rsi = void *data
-	push	rdi
+	push	rsi					; Save pointer data
+	push	rdi					; Save pointer list
 	mov		rdi, 16				; rdi = sizeof(t_list) = 16
-	call	malloc wrt ..plt
-	pop		rdi
+	call	malloc wrt ..plt	; Alloc the new elem list
+	pop		rdi					; Get saved list
+	pop		rsi					; Get saved data
 	cmp		rax, 0				; Check if malloc return 0
-	je		return				; 	-> return == 1
+	je		return				; 	-> return
 	mov		[rax], rsi			; new.data = data
 	mov		rcx, [rdi]			; rcx = *begin_list
 	mov		[rax + 8], rcx		; new.next = *begin_list
