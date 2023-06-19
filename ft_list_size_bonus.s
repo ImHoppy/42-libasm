6bee3374a0ae717ce7006918812cec5811df783c
Hoppy <mbraets@student.42.fr>
2023-06-19 13:29:25 +0200

    Add comment and fix ft_list_size

--- ft_list_size_bonus.s
+++ ft_list_size_bonus.s
@@ -1,14 +1,17 @@
 global ft_list_size
 
 ft_list_size: ; rdi = t_list *begin_list
-	xor		rax, rax
-	jmp		loop
+	mov		rsi, rdi            ; Save pointer list
+	xor		rax, rax			; Set rax to 0
+	jmp		loop				; Start loop
 
 loop:
-	cmp		rdi, 0
-	jne		return
-	inc		rax
-	mov		rdi, [rdi + 8]
+	cmp		rdi, 0				; if (rdi == NULL)
+	je		return				;	-> return rax
+	inc		rax					; rax++
+	mov		rdi, [rdi + 8]		; Move rdi->next to rdi
+	jmp		loop
 
 return:
+	mov		rdi, rsi            ; Return saved pointer
 	ret
